941a285851c1c7595d95831a6c7dee040432b42d
Christian Martinez <christian.martinez@live.com>
2021-07-11 13:58:49 +0000

    removing uploaded istio

--- senv.sh
+++ senv.sh
@@ -1,1 +1,5 @@
+set +o noclobber
+chmod +x patch.sh
+chmod +x kindlocalreg.sh
+curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.10.2 TARGET_ARCH=x86_64 sh -
 export PATH=/workspaces/istio/istio-1.10.2/bin:$PATH
